31d44d08efb410f34d186becc5a681ba2955e13b
unknown <tumbalinajja1@gmail.com>
2022-03-20 22:13:51 +0700

    Updates

---
+++ precache-manifest.fa6dca9d48fe2111c2f5aee79ce70d51.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "89889688147bd7575d6327160d64e760.svg"
+  },
+  {
+    "url": "e18bbf611f2a2e43afc071aa2f4e1512.ttf"
+  },
+  {
+    "url": "f4769f9bdb7466be65088239c12046d1.eot"
+  },
+  {
+    "revision": "095d1bc1e542a289c67802610aaf5654",
+    "url": "index.html"
+  },
+  {
+    "url": "main.cdc1e6b30a8a6a8fb722.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.db4386419004b34dc9dfb842e3521904.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.fa6dca9d48fe2111c2f5aee79ce70d51.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
