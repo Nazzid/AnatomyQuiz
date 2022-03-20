8e383e4f7e84021136c0400c3253b914f65edb0c
unknown <tumbalinajja1@gmail.com>
2022-03-20 22:06:34 +0700

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.9ae0129fdbd3ce5ab2e0c3e1f5eaeafe.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.db4386419004b34dc9dfb842e3521904.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
